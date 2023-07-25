e939e0351523893c613fb1f115cbcfe284e56868
0xproflupin <mac.anvit@gmail.com>
2023-07-25 23:43:17 +0530

    remove signtx

--- example-dapp/src/App.tsx
+++ example-dapp/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react';
 import styled from 'styled-components';
-import { useWallet, useConnection, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
+import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import type { Adapter } from '@solana/wallet-adapter-base';
@@ -8,19 +8,10 @@ import { type SolanaSignInInput } from '@solana/wallet-standard-features';
 import { verifySignIn } from '@solana/wallet-standard-util';
 
 import {
-  createAddressLookupTable,
   createSignInData,
   createSignInErrorData,
-  createTransferTransaction,
-  createTransferTransactionV0,
-  extendAddressLookupTable,
-  pollSignatureStatus,
-  signAllTransactions,
-  signAndSendTransaction,
-  signAndSendTransactionV0WithLookupTable,
   signMessage,
   signIn,
-  signTransaction,
 } from './utils';
 
 import { TLog } from './types';
@@ -62,8 +53,7 @@ export type ConnectedMethods =
 
 
 const StatelessApp = () => {
-  const { wallet, publicKey, connect, disconnect, signMessage: signMsg, signIn: siws, signTransaction: signTx, signAllTransactions: signAllTx, sendTransaction: sendTx } = useWallet();
-  const { connection } = useConnection();
+  const { wallet, publicKey, connect, disconnect, signMessage: signMsg, signIn: siws } = useWallet();
   const [logs, setLogs] = useState<TLog[]>([]);
 
   const createLog = useCallback(
@@ -86,166 +76,6 @@ const StatelessApp = () => {
       message: `Connected to account ${publicKey.toBase58()}`,
     });
   }, [createLog, publicKey, wallet]);
-
-  /** SignAndSendTransaction */
-  const handleSignAndSendTransaction = useCallback(async () => {
-    if (!publicKey || !wallet) return;
-
-    try {
-      const transaction = await createTransferTransaction(publicKey, connection);
-      createLog({
-        status: 'info',
-        method: 'signAndSendTransaction',
-        message: `Requesting signature for: ${JSON.stringify(transaction)}`,
-      });
-      const signature = await signAndSendTransaction(transaction, connection, sendTx);
-      createLog({
-        status: 'info',
-        method: 'signAndSendTransaction',
-        message: `Signed and submitted transaction ${signature}.`,
-      });
-      pollSignatureStatus(signature, connection, createLog);
-    } catch (error) {
-      createLog({
-        status: 'error',
-        method: 'signAndSendTransaction',
-        message: error.message,
-      });
-    }
-  }, [connection, createLog, publicKey, sendTx, wallet]);
-
-  /** SignAndSendTransactionV0 */
-  const handleSignAndSendTransactionV0 = useCallback(async () => {
-    if (!publicKey || !wallet) return;
-
-    try {
-      const transactionV0 = await createTransferTransactionV0(publicKey, connection);
-      createLog({
-        status: 'info',
-        method: 'signAndSendTransactionV0',
-        message: `Requesting signature for: ${JSON.stringify(transactionV0)}`,
-      });
-      const signature = await signAndSendTransaction(transactionV0, connection, sendTx);
-      createLog({
-        status: 'info',
-        method: 'signAndSendTransactionV0',
-        message: `Signed and submitted transactionV0 ${signature}.`,
-      });
-      pollSignatureStatus(signature, connection, createLog);
-    } catch (error) {
-      createLog({
-        status: 'error',
-        method: 'signAndSendTransactionV0',
-        message: error.message,
-      });
-    }
-  }, [connection, createLog, publicKey, sendTx, wallet]);
-
-  /** SignAndSendTransactionV0WithLookupTable */
-  const handleSignAndSendTransactionV0WithLookupTable = useCallback(async () => {
-    if (!publicKey || !wallet) return;
-    try {
-      const [lookupSignature, lookupTableAddress] = await createAddressLookupTable(
-        publicKey,
-        connection,
-        sendTx,
-        await connection.getLatestBlockhash().then((res) => res.blockhash)
-      );
-      createLog({
-        status: 'info',
-        method: 'signAndSendTransactionV0WithLookupTable',
-        message: `Signed and submitted transactionV0 to make an Address Lookup Table ${lookupTableAddress} with signature: ${lookupSignature}. Please wait for 5-7 seconds after signing the next transaction to be able to see the next transaction popup. This time is needed as newly appended addresses require one slot to warmup before being available to transactions for lookups.`,
-      });
-      pollSignatureStatus(lookupSignature, connection, createLog);
-      const extensionSignature = await extendAddressLookupTable(
-        publicKey,
-        connection,
-        await connection.getLatestBlockhash().then((res) => res.blockhash),
-        lookupTableAddress,
-        sendTx
-      );
-      createLog({
-        status: 'info',
-        method: 'signAndSendTransactionV0WithLookupTable',
-        message: `Signed and submitted transactionV0 to extend Address Lookup Table ${extensionSignature}.`,
-      });
-      pollSignatureStatus(extensionSignature, connection, createLog);
-      const signature = await signAndSendTransactionV0WithLookupTable(
-        publicKey,
-        connection,
-        await connection.getLatestBlockhash().then((res) => res.blockhash),
-        lookupTableAddress,
-        sendTx
-      );
-      createLog({
-        status: 'info',
-        method: 'signAndSendTransactionV0WithLookupTable',
-        message: `Signed and submitted transactionV0 with Address Lookup Table ${signature}.`,
-      });
-      pollSignatureStatus(signature, connection, createLog);
-    } catch (error) {
-      createLog({
-        status: 'error',
-        method: 'signAndSendTransactionV0WithLookupTable',
-        message: error.message,
-      });
-    }
-  }, [connection, createLog, publicKey, sendTx, wallet]);
-
-  /** SignTransaction */
-  const handleSignTransaction = useCallback(async () => {
-    if (!publicKey || !wallet) return;
-
-    try {
-      const transaction = await createTransferTransaction(publicKey, connection);
-      createLog({
-        status: 'info',
-        method: 'signTransaction',
-        message: `Requesting signature for: ${JSON.stringify(transaction)}`,
-      });
-      const signedTransaction = await signTransaction(transaction, signTx);
-      createLog({
-        status: 'success',
-        method: 'signTransaction',
-        message: `Transaction signed: ${JSON.stringify(signedTransaction)}`,
-      });
-    } catch (error) {
-      createLog({
-        status: 'error',
-        method: 'signTransaction',
-        message: error.message,
-      });
-    }
-  }, [connection, createLog, publicKey, signTx, wallet]);
-
-  /** SignAllTransactions */
-  const handleSignAllTransactions = useCallback(async () => {
-    if (!publicKey || !wallet) return;
-
-    try {
-      const transactions = [
-        await createTransferTransaction(publicKey, connection),
-        await createTransferTransaction(publicKey, connection),
-      ];
-      createLog({
-        status: 'info',
-        method: 'signAllTransactions',
-        message: `Requesting signature for: ${JSON.stringify(transactions)}`,
-      });
-      const signedTransactions = await signAllTransactions(transactions[0], transactions[1], signAllTx);
-      createLog({
-        status: 'success',
-        method: 'signAllTransactions',
-        message: `Transactions signed: ${JSON.stringify(signedTransactions)}`,
-      });
-    } catch (error) {
-      createLog({
-        status: 'error',
-        method: 'signAllTransactions',
-        message: error.message,
-      });
-    }
-  }, [connection, createLog, publicKey, signAllTx, wallet]);
 
   /** SignMessage */
   const handleSignMessage = useCallback(async () => {
@@ -349,26 +179,6 @@ const StatelessApp = () => {
   const connectedMethods = useMemo(() => {
     return [
       {
-        name: 'Sign and Send Transaction (Legacy)',
-        onClick: handleSignAndSendTransaction,
-      },
-      {
-        name: 'Sign and Send Transaction (v0)',
-        onClick: handleSignAndSendTransactionV0,
-      },
-      {
-        name: 'Sign and Send Transaction (v0 + Lookup table)',
-        onClick: handleSignAndSendTransactionV0WithLookupTable,
-      },
-      {
-        name: 'Sign Transaction',
-        onClick: handleSignTransaction,
-      },
-      {
-        name: 'Sign All Transactions',
-        onClick: handleSignAllTransactions,
-      },
-      {
         name: 'Sign Message',
         onClick: handleSignMessage,
       },
@@ -386,11 +196,6 @@ const StatelessApp = () => {
       },
     ];
   }, [
-    handleSignAndSendTransaction,
-    handleSignAndSendTransactionV0,
-    handleSignAndSendTransactionV0WithLookupTable,
-    handleSignTransaction,
-    handleSignAllTransactions,
     handleSignMessage,
     handleSignIn,
     handleSignInError,
